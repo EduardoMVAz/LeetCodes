ff49b347acbfcf3ba7023572dca53c8ab1928a7f
Eduardo Mendes Vaz <101075804+EduardoMVAz@users.noreply.github.com>
2023-11-03 17:40:13 -0300

    Time: 157 ms (39.68%), Space: 26.5 MB (47.62%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.c
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.c
@@ -1,3 +1,22 @@
 int findCenter(int** edges, int edgesSize, int* edgesColSize) {
-    return edges[0][0] == edges[1][1] || edges[0][0] == edges[1][0] ? edges[0][0] : edges[0][1];
+    bool center = true, hasedge = false;
+    for (int i = 1; i < edgesSize + 2; i++) {
+        for (int j = 0; j < edgesSize; j++) {
+            for (int k = 0; k < edgesColSize[j]; k++) {
+                if (i == edges[j][k]) {
+                    hasedge = true;
+                }
+            }
+            if (!hasedge) {
+                center = false;
+                break;
+            }
+            hasedge = false;
+        }
+        if (center) {
+            return i;
+        }
+        center = true;
+    }
+    return -1;
 }
